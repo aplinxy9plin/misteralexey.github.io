2370e5887206039ac8b32ea0622695027443cd60
MisterAlexey <44365727+MisterAlexey@users.noreply.github.com>
2018-10-31 19:38:53 +0700

    Седьмое занятие

---
+++ 6/2 part/script.js
@@ -0,0 +1,9 @@
+function check() {
+	var Login = document.getElementById('Login').value	
+	var Password = document.getElementById('Password').value
+	if (Login == "qwe" && Password == "123") 
+		{alert('Верно')}
+	else {
+		alert('Неверно')
+	}
+}
