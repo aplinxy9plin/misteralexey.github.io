2c50b4fcacac66c6ac23831cde53e5d7f19e5560
MisterAlexey <44365727+MisterAlexey@users.noreply.github.com>
2018-11-05 19:37:42 +0700

    Девятое занятие

---
+++ 9/script.js
@@ -0,0 +1,17 @@
+//for (var i = 1; i <= 15; i++) {
+//	console.log(i)
+//}
+//var z = 2;
+//for (var i = 1; i < 5; i++) {
+//	console.log (z)
+//	z *= 2
+//}
+function rand() {
+	var max = 3, min = 0;
+	var chislo = Math.floor(Math.random() * (max- min)) + min 
+	return chislo
+}
+
+function game(user) {
+	
+}
