048df3b508523d08b360a6a4099cc0c719b561bf
MisterAlexey <44365727+MisterAlexey@users.noreply.github.com>
2018-11-07 22:34:17 +0700

    Рабочая версия КаНоБу

--- 9/script.js
+++ 9/script.js
@@ -1,36 +1,60 @@
 function rand(){
 	var max = 3, min = 0;
 	var chislo = Math.floor(Math.random() * (max - min)) + min
-	return chislo
+	if (chislo == 0) {
+		document.getElementById('comp_pic').src = "kam.png"
+	}
+	if (chislo == 1) {
+		document.getElementById('comp_pic').src = "nozh.png"
+	}
+	if (chislo == 2) {
+		document.getElementById('comp_pic').src = "bum.png"
+	}
+	return chislo	
 }
 
+var winner = 0;
+var looser = 0;
+var nich = 0;
 function game(user){
 	var chislo = rand()
-        if (user === chislo) {
-            alert ("The result is a tie!")
+	
+
+        if (user == chislo) {        	
+        	alert ("Ничья!")
+        	nich +=1;
+			document.getElementById("tie").innerHTML = nich
         }
-        else if(user === "0") {
-            if(chislo === "1") {
-                alert ("rock wins")
-            }
-            else {
-                alert ("paper wins")
-            }
+        if (user == 0 && chislo == 1) {        	
+        	alert ("Ты победил! Твой камень ломает ножницы!")
+        	winner +=1;
+			document.getElementById("wins").innerHTML = winner
         }
-        else if(user === "2") {
-            if(chislo === "0") {
-                alert ("paper wins")
-            }
-            else {
-                alert ("scissors wins")
-            }
+        if (user == 0 && chislo == 2) {
+        	alert ("Ты проиграл! Твоя бумага победила камень!")
+        	looser +=1;
+			document.getElementById("looses").innerHTML = looser
         }
-        else if(user === "1") {
-            if(chislo === "2") {
-                alert ("scissors wins")
-            }
-            else {
-                alert ("rock wins")
-            }
+        if (user == 1 && chislo == 0) {
+        	alert ("Ты проиграл! Камень ломает твои ножницы!")
+        	looser +=1;
+			document.getElementById("looses").innerHTML = looser
         }
-}
+        if (user == 1 && chislo == 2) {
+        	alert ("Ты победил! Твои ножницы режут бумагу!")
+        	winner +=1;
+			document.getElementById("wins").innerHTML = winner
+        }
+        if (user == 2 && chislo == 0) {
+        	alert ("Ты победил! Твоя бумага побеждаем камень!")
+        	winner +=1;
+			document.getElementById("wins").innerHTML = winner
+        }
+        if (user == 2 && chislo == 1) {
+        	alert ("Ты проиграл! Твою бумагу порезали ножницами!")
+        	looser +=1;
+			document.getElementById("looses").innerHTML = looser
+        }
+    }
+
+		
