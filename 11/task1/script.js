41d8723d1f86d7b5f925274bc6f25708332dd56d
MisterAlexey <44365727+MisterAlexey@users.noreply.github.com>
2018-11-18 23:48:31 +0700

    Дз - 11

---
+++ 11/task1/script.js
@@ -0,0 +1,7 @@
+var arr = [3.5, 2.3, -12.34, -6.1, 5.3, 9.12, -9.12, 3.5, -1.1, 8.612]
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+	sum += arr[i];
+	console.log(sum)
+}
+var result = sum / arr.length;
